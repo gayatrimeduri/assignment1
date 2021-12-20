382c2db5c8b243eef4a6cdacaed7b0f52c724b17
“gayatrimeduri” <gayatri62193@gmail.com>
2021-12-20 11:05:04 +0530

    First commit

---
+++ question3a.js
@@ -0,0 +1,15 @@
+function funpink() {
+    document.getElementById("text1").style.color ="pink";
+}
+function funsblue(){
+   document.getElementById("text2").style.color="skyblue";
+}
+function funindigo(){
+   document.getElementById("text3").style.color="indigo";
+}
+function funorange(){
+   document.getElementById("text4").style.color="orange";
+}
+function funviolet(){
+   document.getElementById("text5").style.color="violet";
+}
